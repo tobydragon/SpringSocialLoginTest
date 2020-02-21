2453bd2c348e54880a95313d00401fc188afb61a
Toby Dragon <tdragon@ithaca.edu>
2020-02-21 10:30:40 -0500

    initial commit of working simple github login

---
+++ src/main/java/edu/ithaca/dragon/SpringSocialLoginTest/SpringSocialLoginTestApplication.java
@@ -0,0 +1,13 @@
+package edu.ithaca.dragon.SpringSocialLoginTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialLoginTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialLoginTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ithaca/dragon/SpringSocialLoginTest/SpringSocialLoginTestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ithaca.dragon.SpringSocialLoginTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialLoginTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
