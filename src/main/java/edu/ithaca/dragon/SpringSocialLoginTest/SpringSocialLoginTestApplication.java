2c89b8feac3b52072ce6a2ea15084a6ab3aad62d
Toby Dragon <tdragon@ithaca.edu>
2020-02-25 13:48:57 -0500

    added endpoint to print the info about the user gotten from the social login app

--- src/main/java/edu/ithaca/dragon/SpringSocialLoginTest/SpringSocialLoginTestApplication.java
+++ src/main/java/edu/ithaca/dragon/SpringSocialLoginTest/SpringSocialLoginTestApplication.java
@@ -2,9 +2,23 @@ package edu.ithaca.dragon.SpringSocialLoginTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class SpringSocialLoginTestApplication {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSocialLoginTestApplication.class, args);
